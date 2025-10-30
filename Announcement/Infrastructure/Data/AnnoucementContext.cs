80be22e16aa619c0d3c17e196112937cfc03de5e
Дарина Стаценко <darinastat7@gmail.com>
2025-10-30 16:44:54 +0300

    #1 initial commit

---
+++ Announcement/Infrastructure/Data/AnnoucementContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class AnnoucementContext : DbContext
+    {
+        public AnnoucementContext(DbContextOptions<AnnoucementContext> options): base(options) { }
+
+        DbSet<Announcement> Announcements { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Announcement>(entity => 
+            { 
+                entity.HasKey(e => e.Id);
+            });
+        }
+    }
+}
